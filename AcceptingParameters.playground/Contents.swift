69b91db374125f13d8b1d9989aaf6f0d82c16963
Scott Doggett <sdoggett@lcs.on.ca>
2021-01-12 13:08:56 -0500

    Accepting Parameters playground complete with comments

---
+++ AcceptingParameters.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Cocoa
+
+print("Hello, world!")
+
+
+// Name functions square, parameter name is number and parameter type is int
+func square(number: Int) {
+    
+    // Here we use to parameter in our functions
+    print(number * number)
+}
+
+// Calling our functions with the number parameter = 8
+square(number: 8)
