6876b7648ebf76ea09abb646133315fb2bd970e3
Scott Doggett <sdoggett@lcs.on.ca>
2021-01-15 16:23:47 -0500

    I created a file of notes I took while watching the video "Programming with Purpose" by Ish Shabazz.

---
+++ Notes From Video - Programming with Purpose.swift
@@ -0,0 +1,37 @@
+//
+// Notes From Video.swift
+// Notes
+//
+// Created by Doggett, Scott on 2021-01-15.
+//
+
+import Foundation
+
+
+//MARK: NOTES
+
+// 00:58 "My greater purpose in life is to help as many people as I can in small ways"
+
+// Very tough childhood. Grew up in a tough neighbourhood in the 1980s. Goal every day was to survive. Wanted to learn about computers at a young age. The teacher worked hard to make sure she could teach him about computers. He wanted to learn all the time. Shifted goal to help others in small ways.
+
+// 06:20 "Little things become big things over time"
+
+// 06:39 Curiosity. Start with curiosity.
+
+// 07:19 "Stop knowing so much and listen, being with listening"
+
+// 11:59 Curiosity weakens fear. Asking "and then what" is very useful. Examine your fear. Ask "and then what" until your fear has nothing left to say.
+
+// 14:22 "Fake it till you make it"
+
+// 15:46 "Big things start out small"
+
+// 15:59 "There is no such thing as spare time, we have a finite amount of time and we won't be getting any extra"
+
+// 16:24 Ms. Mead wasn't even interested in computers. She gave him just enough information to get him started. She set him on the right track.
+
+// 17:38 "Be present. You never get to have this moment again, so make the most out of it"
+
+// 21:43 "What if all the science-fiction I read is true, and time is not linear? What if everything that's happening now has already happened, and the result is already set? What if what I'm doing now is just having an extremely vivid recollection of the past? What would I do differently? I would slow down. I would find small things to appreciate."
+
+// 23:00 "Life is really short. If you can live it like you've gotten a second chance to do it again, it's amazing how much more alive you feel"
