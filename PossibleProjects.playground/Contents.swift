6736c32f20073564b2216af0dc357c2416cf37d6
Scott Doggett <sdoggett@lcs.on.ca>
2021-01-26 10:03:48 -0500

    Idea added for new project.

---
+++ PossibleProjects.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Cocoa
+
+//MARK: POSSIBLE PROJECTS
+
+//MARK: FINANCE TRACKER
+
+// This would be an app that tracks your financial purchaes and shows you a list and graph of what you spend your money on.
+
+// Add on: The user could set a monthly spending goal and it would compare how much they are spending to the goal
+
+// Add on: The user sets spending goals for each catagory and compares their monthly spending in each catagory
+
+//MARK: other ideas??
