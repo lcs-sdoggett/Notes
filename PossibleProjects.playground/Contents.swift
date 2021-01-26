58e26672f669fcc91a79fe1dc1e73f36a62d4172
Scott Doggett <sdoggett@lcs.on.ca>
2021-01-26 17:40:35 -0500

    Fixed spelling

--- PossibleProjects.playground/Contents.swift
+++ PossibleProjects.playground/Contents.swift
@@ -17,3 +17,9 @@ import Cocoa
 // They would then input their daily screen time, and the app would give them feedback regarding if they went over or under, and would tell them how to improve
 
 // Not exactly sure how this would be different from the screen time app built into settings, but I think it would be focused around helping the user cut down on screen time
+
+//MARK: LCS EATS
+
+// When LCS EATS was running, we were taking orders through a google sheet that automatically calcuated the prices, tax and delivery cost of every order. The app would do this, without needing it to be in person.
+
+// The customer would order through the app, and the data of the order would be sent to an LCS EATS employee. The customer could also see any old transactions they have made.
